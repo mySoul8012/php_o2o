82a7bb178d3a924430d451d7f6ad74288da70f9f
ming <mingming@mingming.email>
2019-06-11 01:27:13 +0800

    :construction:  construction

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -1,13 +1,18 @@
 <?php
 namespace app\admin\controller;
 
+use Email;
 use Map;
 use think\facade\View;
 class Index extends BaseController
 {
     public function index(){
-
         return View::fetch();
+    }
+
+    public function test(){
+        $email = app("Email");
+        $email->sendMail();
     }
 
 
