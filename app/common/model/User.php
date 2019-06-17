0e533a8d16250c0aa36a10ca23a54167b450bb23
ming <mingming@mingming.email>
2019-06-17 15:30:36 +0800

    :construction:  construction

---
+++ app/common/model/User.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace app\common\model;
+
+
+use think\Exception;
+use think\Model;
+
+class User extends Model
+{
+    public function add($data){
+
+        $data['status'] = 1;
+        unset($data['repassword']);
+        unset($data['verifyCode']);
+
+        return $this->insert($data);
+    }
+
+    public function getUserByUsername($username){
+        $data = [
+            'username' => $username
+        ];
+
+        return $this->where($data)->find();
+    }
+
+    public function updateById($data, $id){
+        return $this->allowField(true)->update($data, [
+            'id' => $id
+        ]);
+    }
+}
--- app/index/controller/User.php
+++ app/index/controller/User.php
@@ -15,14 +15,63 @@ class User extends BaseController
         return View::fetch();
     }
 
+    public function logincheck(){
+        // 判断
+        if(!$this->request->isPost()){
+            return View::fetch('提交非法');
+        }
+
+        $data = input('post.');
+
+        // 判断是否相同
+        $user =  app('app\common\model\User')->getUserByUsername($data['userName']);
+
+        if(!$user || $user->status != 1){
+            return View::fetch('error');
+        }
+
+        // 判断密码
+        if(md5($data['password'] . $user->code )!= $user->password){
+            // 失败
+            return View::fetch('error');
+        }
+
+        // 登录成功
+        app('app\common\model\User')->updateById([
+            'last_login_time' => time(),
+        ], $user->id);
+
+        // 保存session
+        session('o2o_user', (String)$user->id);
+
+        redirect(url('index/index'));
+    }
+
     /**
      * @return string
      */
     public function register(){
-        if(!$this->request->isPost()){
+        if($this->request->isPost()){
             $data = input('post.');
-            if(captcha_check($data['verifyCode'])){
-                echo 4;
+            if(!captcha_check($data['verifyCode'])){
+                return View::fetch('error');
+            }else{
+                // 判断两次密码
+                if($data['password'] != $data['repassword']){
+                    return View::fetch('error');
+                }
+
+                $data['last_login_ip'] = $this->request->ip();
+                $data['last_login_time'] = time();
+                $data['code'] = mt_rand(100,100000);
+                $data['password'] = md5($data['password'] . $data['code']);
+
+                $res  = app('app\common\model\User')->add($data);
+                if($res) {
+                    return View::fetch('success');
+                }else{
+                    return View::fetch('error');
+                }
             }
         }
         return View::fetch();
